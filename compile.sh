a0e5e45b32f2bef0b5951e3082258826199a964b
Kai Weeks <kai.weeks@student-ltu.se>
2019-04-19 15:51:52 -0700

    Ensures compile script creates required bin directory

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 echo "Compiling code..."
+mkdir -p ./bin
 javac -d ./bin -cp ./src ./src/ltu/Main.java ./src/ltu/*.java
 echo "Compiling tests..."
 javac -d ./bin -cp ./lib/org.junit4-4.3.1.jar:./src ./src/tests/ltu/*.java
